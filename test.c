cdf17f0d56664400b935e1cd2ed75e69cf4a6426
WuM3 <91886168+WuM3@users.noreply.github.com>
2021-10-07 16:08:07 +0800

    Add files via upload

---
+++ test.c
@@ -0,0 +1,42 @@
+//#define _CRT_SECURE_NO_WARNINGS 1
+//
+//#include <stdio.h>
+//int main()
+//{
+//	int a = 0;
+//	int c = 0;
+//	int sum = 0;
+//	scanf("%d %d", &a, &c);
+//	sum = a + c;
+//	printf("sum=%d\n", sum);
+//	return 0;
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//	printf("¹þ¹þ");
+//	return 0;
+//}
+//#include <stdio.h>
+//extern a;
+//int main()
+//{
+//	printf("%d\n", a);
+//	return 0;
+//}
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+int main()
+{
+	float a = 0;
+	float b = 0;
+	float c = 0;
+	float sum = 0;
+	scanf("%f %f %f", &a, &b, &c);
+	sum = a + b + c;
+	printf("sum=%f\n", sum);
+
+
+	return 0;
+}
